cd8c2ebd331d10dc901e279a6565853d2c3a7865
med-li-jr <li.med.jr@gmail.com>
2021-06-02 22:04:27 -0700

    Try a first optimise code by creating a service class

---
+++ LearnSelenium/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LearnSelenium
+{
+    public class EmailService
+    {
+
+        public static IWebDriver Driver;
+        public static ChromeOptions Options = new ChromeOptions() { DebuggerAddress = "localhost:9014" };
+        public static string PathDriverChrome = @"D:\DocStream";
+        public static void SendMail(string sendTo, string objectMail, string messageMail)
+        {
+            if(Driver == null)
+                Driver = new ChromeDriver(PathDriverChrome, Options);
+    
+            Driver.Navigate().GoToUrl("https://mail.google.com/mail/u/0/h/");
+
+            var btnNewMessage = Driver.FindElement(By.XPath("/html/body/table[2]/tbody/tr/td[1]/table[1]/tbody/tr[1]/td/b/a"));
+            btnNewMessage.Click();
+
+            var emailTo = Driver.FindElement(By.Id("to"));
+            emailTo.SendKeys(sendTo);
+
+            var xpathSubject = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[4]/td[2]/input";
+            var objectEmail = Driver.FindElement(By.Name("subject"));
+            objectEmail.SendKeys(objectMail);
+
+            var xpathMessage = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[8]/td[2]/textarea";
+            var messageEmail = Driver.FindElement(By.Name("body"));
+            messageEmail.SendKeys(messageMail);
+
+            var xpathBtnSend = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[3]/tbody/tr/td/input[1]";
+            var btnSendNewMessage = Driver.FindElement(By.Name("nvp_bu_send"));
+            Thread.Sleep(2000);
+            btnSendNewMessage.Click();
+
+        }
+    }
+}
--- LearnSelenium/Program.cs
+++ LearnSelenium/Program.cs
@@ -1,69 +1,20 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Threading;
+﻿using System;
+
 
 namespace LearnSelenium
 {
     class Program
     {
 
-        public static IWebDriver driver;
         static void Main(string[] args)
         {
-            ChromeOptions options = new ChromeOptions();
-            //options.DebuggerAddress = "localhost:9014";
-            driver = new ChromeDriver(@"D:\DocStream", options);
-            driver.Navigate().GoToUrl("https://mail.google.com/mail/u/0/h/");
-            var btnNewMessage = driver.FindElement(By.XPath("/html/body/table[2]/tbody/tr/td[1]/table[1]/tbody/tr[1]/td/b/a"));
 
+            //Do not forget that cmd line 
+            // chrome -remote-debugging-port=9014 --user-data-dir="D:\DocStream\Selenium"
 
-            btnNewMessage.Click();
+            EmailService.SendMail("mohamedlawani2107@gmail.com", "test class service", "new test again right");
 
-
-            var emailTo = driver.FindElement(By.Id("to"));
-            emailTo.SendKeys("mohamedlawani2107@gmail.com");
-            var xpathSubject = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[4]/td[2]/input";
-            var objectEmail = driver.FindElement(By.Name("subject"));
-            objectEmail.SendKeys("Who Knows right Email");
-            var xpathMessage = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[8]/td[2]/textarea";
-            var messageEmail = driver.FindElement(By.Name("body"));
-            messageEmail.SendKeys("C'cmt man Test Email for who knows right");
-
-            var xpathBtnSend = "/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[3]/tbody/tr/td/input[1]";
-            var btnSendNewMessage = driver.FindElement(By.Name("nvp_bu_send"));
-            btnSendNewMessage.Click();
-            //var btnEnglish = driver.FindElement(By.XPath("//*[@id=\"SIvCob\"]/a[2]"));
-            //btnEnglish.Click();
-            //            var btnSignIn = driver.FindElement(By.XPath("//*[@id=\"gb\"]/div/div[2]/a"));
-
-            //Thread.Sleep(2000);
-            //var btnSignInFox = driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div/div/div[2]/a"));
-            //var btnSignInFoxStack = driver.FindElement(By.XPath("/html/body/header/div/ol[2]/li[2]/a[2]"));
-            ////Thread.Sleep(3000);
-            //btnSignInFoxStack.Click();
-            //var btnSigInGoogleStackFox = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div[2]/div[2]/button[1]"));
-            //btnSigInGoogleStackFox.Click();
-            ////var inputEmail = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            ////var inputEmailFox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            //var inputEmailStackFox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            ////Thread.Sleep(2000);
-            //inputEmailStackFox.SendKeys("vj.junior799@gmail.com");
-            ////var btnNext = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]"));
-            ////var btnNextFox = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]"));
-            //var btnNextStackGoogleFox = driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button"));
-            ////Thread.Sleep(3000);
-            //btnNextStackGoogleFox.Click();
-            //var inputPwdStackFox = driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input"));
-            ////Thread.Sleep(2000);
-            //inputPwdStackFox.SendKeys("!Bonjour!Google!Hein!Mew!2018!");
-            //var btnNextStackGooglePwdFox = driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button"));
-            ////Thread.Sleep(3000);
-            //btnNextStackGooglePwdFox.Click();
-            //Console.WriteLine("Hello World!");
+            
         }
     }
 }
